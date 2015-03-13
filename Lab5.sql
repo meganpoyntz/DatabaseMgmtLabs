834b7d7c8403598a0dce53e193ac4e18a98e1483
meganpoyntz <fightinirish10x3@148-100-156-179.FoxNet.marist.edu>
2015-03-12 21:04:58 -0400

    Uploading Database Mgmt Lab 5
    Final Commit

--- Lab5.sql
+++ Lab5.sql
@@ -17,34 +17,34 @@ INNER JOIN products p ON o2.pid = p.pid
 ORDER BY p.pid
 
 -- Question 3: Show the names of customers who have never placed an order. Use a subquery.
-SELECT name
+SELECT name AS "Customer Name"
 FROM customers
 WHERE cid not in (
 	SELECT distinct cid
 	FROM orders)
 
 -- Question 4: Show the names of customers who have never placed an order. Use an outer join.
-SELECT c.name
+SELECT c.name AS "Customer Name"
 FROM customers c
 LEFT OUTER JOIN orders o ON o.cid=c.cid
 WHERE ordno is NULL
 
 -- Question 5: Show the names of customers who placed at least one order through an agent in their own city, along with those agent(s') names.
-SELECT distinct c.name, a.name
+SELECT distinct c.name AS "Customer Name", a.name AS "Agent Name"
 FROM customers c
 INNER JOIN orders o ON c.cid = o.cid
 INNER JOIN agents a ON a.city = c.city
 
 -- Question 6: Show the names of customers and agents living in the same city, along with the name of the shared city,
 -- regardless of whether or not the customer has ever placed an order with that agent.
-SELECT distinct c.name, a.name, c.city
+SELECT distinct c.name AS "Customer Name", a.name AS "Agent Name", c.city
 FROM customers c, agents a
 WHERE c.city = a.city
 ORDER BY city;
 
 -- Question 7: Show the name and city of customers who live in the city that makes the fewest different kinds of products.
 -- (Hint: Use count and group by on the Products table.)
-SELECT c.name, c.city
+SELECT c.name AS "Customer Name", c.city
 FROM customers c
 WHERE c.city in (
 	SELECT p.city
